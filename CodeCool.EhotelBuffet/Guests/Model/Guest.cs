896c34d68c2e82aef0a72612ba8a477c455bcc89
Maria <simam9520@gmail.com>
2023-03-14 13:53:12 +0200

    aftermerge

--- CodeCool.EhotelBuffet/Guests/Model/Guest.cs
+++ CodeCool.EhotelBuffet/Guests/Model/Guest.cs
@@ -4,21 +4,28 @@ namespace CodeCool.EhotelBuffet.Guests.Model;
 
 public record Guest(string Name, GuestType GuestType)
 {
-    public MealType[] MealPreferences { get; } = GetMeal(GuestType);
+    public MealType[] MealPreferences { get; } = SetMealPreferences(GuestType);
 
-
-    public static MealType[] GetMeal(GuestType guest)
+    public static MealType[] SetMealPreferences(GuestType guest)
     {
-
         switch (guest)
         {
+            case GuestType.Business:
+            {
+                return new MealType[] { MealType.Bun};
+            }
             case GuestType.Kid:
             {
-                return new MealType[] { MealType.Bun, MealType.Cereal };
+                return new MealType[] { MealType.Cereal };
             }
-
+            case GuestType.Tourist:
+            {
+                return new MealType[] { MealType.Croissant };
+            }
+            default:
+            {
+                return Array.Empty<MealType>();
+            }
         }
-
-        return Array.Empty<MealType>();
     }
 }
